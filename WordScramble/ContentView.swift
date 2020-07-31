04c880c5aebcab0d039f603a6ae7cedb5b5ddd42
artsasse <30945018+artsasse@users.noreply.github.com>
2020-07-31 15:15:43 -0300

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Arthur Mendonça Sasse on 31/07/20.
+//  Copyright © 2020 Arthur Mendonça Sasse. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
